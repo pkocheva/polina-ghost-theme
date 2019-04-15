d7272c7e9c801f5721790358c54cefdc1c4d51c2
Polina <p.kocheva@gmail.com>
2019-04-15 23:23:42 +0800

    *actually* don't show next city if no next city in spreadsheet (bug was still there)

--- assets/js/trips.js
+++ assets/js/trips.js
@@ -1,10 +1,12 @@
 $.getJSON("https://spreadsheets.google.com/feeds/list/1-eierzHfP9pVOdppzcaR7BBsKWMCS41VOB5ZQF8GmPA/od6/public/values?alt=json", function(data) {
   arr = data.feed.entry;
   var currentCity = '';
-  var nextCity = '';
+  var nextCity = null;
   var nextCityFound = false;
   var nextDate;
   var currentDate = new Date();
+
+
 
   for(var i=0; i<arr.length; i++) {
       var from = [];
@@ -17,8 +19,11 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1-eierzHfP9pVOdppzcaR7BBsK
       to['month'] = arr[i].gsx$to.$t.substr(3, 2); 
       to['year'] = arr[i].gsx$to.$t.substr(6, 7);
 
-      var fromDate = new Date(from['year'], from['month'], from['day']);
-      var toDate = new Date(to['year'], to['month'], to['day']);
+      if( from['day'] > -1 && from['month'] > -1 && from['year'] > -1 ) {
+        var fromDate = new Date(from['year'], from['month'], from['day']);
+      }
+
+      //var toDate = new Date(to['year'], to['month'], to['day']);
 
       //console.log("in " + arr[i].gsx$city.$t + " from: " + fromDate + " to: " + toDate);
       
@@ -26,7 +31,9 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1-eierzHfP9pVOdppzcaR7BBsK
           nextCityFound = true;
           currentCity = arr[i].gsx$city.$t;
           nextCity = arr[i-1].gsx$city.$t;
-          nextDate = new Date(arr[i-1].gsx$from.$t.substr(6, 7), arr[i-1].gsx$from.$t.substr(3, 2)-1, arr[i-1].gsx$from.$t.substr(0, 2))
+          if(arr[i-1].gsx$from.$t != "") {
+            nextDate = new Date(arr[i-1].gsx$from.$t.substr(6, 7), arr[i-1].gsx$from.$t.substr(3, 2)-1, arr[i-1].gsx$from.$t.substr(0, 2));
+          }          
       }
   }
 
@@ -36,7 +43,7 @@ $.getJSON("https://spreadsheets.google.com/feeds/list/1-eierzHfP9pVOdppzcaR7BBsK
       $('#current-city').html(currentCity); 
   }
 
-  if(nextCity) {
+  if(nextDate && nextCity) {
       $('#next-city').html(nextCity);
       $('#next-city-time').html(dateDiff);
   } else {
