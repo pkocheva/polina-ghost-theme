352480e6797a2f6ae06cd8fbb20f43b2231219e7
Polina <p.kocheva@gmail.com>
2019-04-30 19:56:38 +0800

    use nomadlist json instead of deprecated google sheet json

--- assets/js/trips.js
+++ assets/js/trips.js
@@ -1,53 +1,22 @@
-$.getJSON("https://spreadsheets.google.com/feeds/list/1-eierzHfP9pVOdppzcaR7BBsKWMCS41VOB5ZQF8GmPA/od6/public/values?alt=json", function(data) {
-  arr = data.feed.entry;
-  var currentCity = '';
-  var nextCity = null;
-  var nextCityFound = false;
-  var nextDate;
-  var currentDate = new Date();
+$.getJSON("https://nomadlist.com/@polinakocheva.json", function(data) {
 
-
-
-  for(var i=0; i<arr.length; i++) {
-      var from = [];
-      from['day'] = arr[i].gsx$from.$t.substr(0, 2);   
-      from['month'] = arr[i].gsx$from.$t.substr(3, 2) - 1; 
-      from['year'] = arr[i].gsx$from.$t.substr(6, 7);
-
-      var to = [];
-      to['day'] = arr[i].gsx$to.$t.substr(0, 2);   
-      to['month'] = arr[i].gsx$to.$t.substr(3, 2); 
-      to['year'] = arr[i].gsx$to.$t.substr(6, 7);
-
-      if( from['day'] > -1 && from['month'] > -1 && from['year'] > -1 ) {
-        var fromDate = new Date(from['year'], from['month'], from['day']);
-      }
-
-      //var toDate = new Date(to['year'], to['month'], to['day']);
-
-      //console.log("in " + arr[i].gsx$city.$t + " from: " + fromDate + " to: " + toDate);
-      
-      if( currentDate > fromDate && !nextCityFound ) {
-          nextCityFound = true;
-          currentCity = arr[i].gsx$city.$t;
-          nextCity = arr[i-1].gsx$city.$t;
-          if(arr[i-1].gsx$from.$t != "") {
-            nextDate = new Date(arr[i-1].gsx$from.$t.substr(6, 7), arr[i-1].gsx$from.$t.substr(3, 2)-1, arr[i-1].gsx$from.$t.substr(0, 2));
-          }          
-      }
+  nextCity = data.location.next.city + ', ' + data.location.next.country;
+  if(data.location.now.city && data.location.now.country) {
+    currentCity = data.location.now.city + ', ' + data.location.now.country;
+    $('#current-city').html(currentCity); 
   }
 
-  dateDiff = Math.round( (nextDate-currentDate)/(1000*60*60*24) );
-
-  if(currentCity) {
-      $('#current-city').html(currentCity); 
+  if(data.location.next.city && data.location.next.country) {
+    nextCity = data.location.next.city + ', ' + data.location.next.country;
+    $('#next-city').html(nextCity); 
   }
 
-  if(nextDate && nextCity) {
-      $('#next-city').html(nextCity);
-      $('#next-city-time').html(dateDiff);
-  } else {
-      $('#leaving-for').html('');
-  }
+  if(data.location.next.date_start) {
+    var currentDate = moment();
+    var fromDate = moment(data.location.next.date_start, 'YYYY-MM-DD').toDate();
+    var remainingDays = Math.abs(currentDate.diff(fromDate, 'days'));
   
+    $('#next-city-time').html(remainingDays);
+  }
+
 });
